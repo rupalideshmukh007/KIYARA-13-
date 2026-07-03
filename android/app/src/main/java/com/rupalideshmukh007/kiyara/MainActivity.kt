2f11b83ffa4611c0bc79d804c83817874df95828
ridergeming170 <ridergeming170@gmail.com>
2026-07-03 11:19:24 +0000

    Fix: Updated packages and resolved prebuild issues

--- android/app/src/main/java/com/rupalideshmukh007/kiyara/MainActivity.kt
+++ android/app/src/main/java/com/rupalideshmukh007/kiyara/MainActivity.kt
@@ -1,62 +1,61 @@
-
 package com.rupalideshmukh007.kiyara
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 import expo.modules.ReactActivityDelegateWrapper
-import expo.modules.splashscreen.SplashScreenManager
 
 class MainActivity : ReactActivity() {
-
   override fun onCreate(savedInstanceState: Bundle?) {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    // setTheme(R.style.AppTheme);
-    SplashScreenManager.registerOnActivity(this)
+    setTheme(R.style.AppTheme);
     super.onCreate(null)
-
-    // Listen for the custom event to start the headless task
-    val intent = intent
-    if (intent.action == "START_KIYARA_HEADLESS_TASK") {
-        Log.d("MainActivity", "Intent to start headless task received")
-        val serviceIntent = Intent(applicationContext, KiyaraHeadlessTaskService::class.java)
-        val bundle = Bundle()
-        // You can pass data to the headless task here if needed
-        serviceIntent.putExtras(bundle)
-        applicationContext.startService(serviceIntent)
-        HeadlessJsTaskService.acquireWakeLockNow(applicationContext)
-    }
   }
 
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
   override fun getMainComponentName(): String = "main"
 
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
-        this,
-        BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-        object : DefaultReactActivityDelegate(
-            this,
-            mainComponentName,
-            fabricEnabled
-        ) {}
-    )
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
   }
 
+  /**
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
   override fun invokeDefaultOnBackPressed() {
-    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-      if (!moveTaskToBack(false)) {
-        super.invokeDefaultOnBackPressed()
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
       }
-      return
-    }
-    super.invokeDefaultOnBackPressed()
+
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
   }
 }
